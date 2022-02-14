26f44e93f5ce46da82ce47e1324b4a3606e6faa2
ambroise <ambroise.berthe@ikomia.com>
2022-02-14 09:12:24 +0100

    Initial commit

---
+++ infer_detectron2_instance_segmentation_process.py
@@ -0,0 +1,225 @@
+# Copyright (C) 2021 Ikomia SAS
+# Contact: https://www.ikomia.com
+#
+# This file is part of the IkomiaStudio software.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ikomia import core, dataprocess
+import copy
+# Your imports below
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+import numpy as np
+import torch
+
+# --------------------
+# - Class to handle the process parameters
+# - Inherits PyCore.CWorkflowTaskParam from Ikomia API
+# --------------------
+class InferDetectron2InstanceSegmentationParam(core.CWorkflowTaskParam):
+
+    def __init__(self):
+        core.CWorkflowTaskParam.__init__(self)
+        # Place default value initialization here
+        self.model_name = "COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x"
+        self.conf_thres = 0.5
+        self.cuda = True if torch.cuda.is_available() else False
+        self.update = False
+
+
+    def setParamMap(self, param_map):
+        # Set parameters values from Ikomia application
+        # Parameters values are stored as string and accessible like a python dict
+        self.model_name = param_map["model_name"]
+        self.conf_thres = float(param_map["conf_thres"])
+        self.cuda = eval(param_map["cuda"])
+
+    def getParamMap(self):
+        # Send parameters values to Ikomia application
+        # Create the specific dict structure (string container)
+        param_map = core.ParamMap()
+        param_map["model_name"] = self.model_name
+        param_map["conf_thres"] = str(self.conf_thres)
+        param_map["cuda"] = str(self.cuda)
+        return param_map
+
+
+# --------------------
+# - Class which implements the process
+# - Inherits PyCore.CWorkflowTask or derived from Ikomia API
+# --------------------
+class InferDetectron2InstanceSegmentation(dataprocess.C2dImageTask):
+
+    def __init__(self, name, param):
+        dataprocess.C2dImageTask.__init__(self, name)
+        # Add input/output of the process here
+        self.predictor = None
+        self.cfg = None
+        self.setOutputDataType(core.IODataType.IMAGE_LABEL, 0)
+        self.addOutput(dataprocess.CImageIO())
+        # Add graphics output
+        self.addOutput(dataprocess.CGraphicsOutput())
+        # Add numeric output
+        self.addOutput(dataprocess.CBlobMeasureIO())
+        self.addOutput(dataprocess.CImageIO())
+        self.setOutputDataType(core.IODataType.IMAGE_LABEL, 4)
+
+
+        # Create parameters class
+        if param is None:
+            self.setParam(InferDetectron2InstanceSegmentationParam())
+        else:
+            self.setParam(copy.deepcopy(param))
+
+    def getProgressSteps(self, eltCount=1):
+        # Function returning the number of progress steps for this process
+        # This is handled by the main progress bar of Ikomia application
+        return 1
+
+    def run(self):
+        # Core function of your process
+        # Call beginTaskRun for initialization
+        self.beginTaskRun()
+        self.forwardInputImage(0, 1)
+
+
+        # Get parameters :
+        param = self.getParam()
+        if self.predictor is None or param.update:
+            self.cfg = get_cfg()
+            self.cfg.merge_from_file(model_zoo.get_config_file(param.model_name + '.yaml'))
+            self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = param.conf_thres
+            self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(param.model_name + '.yaml')
+            self.class_names = MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]).get("thing_classes")
+            self.cfg.MODEL.DEVICE = 'cuda' if param.cuda else 'cpu'
+            self.predictor = DefaultPredictor(self.cfg)
+            param.update = False
+            print("Inference will run on " + ('cuda' if param.cuda else 'cpu'))
+
+        # Get input :
+        input = self.getInput(0)
+        instance_out = self.getOutput(0)
+        outpout_img = self.getOutput(1)
+        graphics_output = self.getOutput(2)
+        numeric_output = self.getOutput(3)
+        panoptic_out = self.getOutput(4)
+        if input.isDataAvailable():
+            img = input.getImage()
+
+            graphics_output.setNewLayer("Detectron2_Instance_Segmentation")
+            graphics_output.setImageIndex(1)
+            numeric_output.clearData()
+
+            instance_img,panoptic_img = self.infer(img,graphics_output,numeric_output)
+            instance_out.setImage(instance_img)
+            panoptic_out.setImage(panoptic_img)
+
+        # Step progress bar:
+        self.emitStepProgress()
+
+        # Call endTaskRun to finalize process
+        self.endTaskRun()
+
+    def infer(self, img, graphics_output, numeric_output):
+        outputs = self.predictor(img)
+        h, w, c = np.shape(img)
+        panoptic_seg = torch.full((h, w), fill_value=-1)
+        instance_seg = torch.full((h, w), fill_value=-1)
+
+        if "instances" in outputs.keys():
+            instances = outputs["instances"].to("cpu")
+            scores = instances.scores
+            boxes = instances.pred_boxes
+            classes = instances.pred_classes
+            masks = instances.pred_masks
+            nb_instance = 0
+            for box, score, cls, mask in zip(boxes, scores, classes, masks):
+                if score >= self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST:
+                    nb_instance +=1
+                    panoptic_seg[mask] = cls+1
+                    instance_seg[mask] = nb_instance
+                    x1, y1, x2, y2 = box.numpy()
+                    cls = int(cls.numpy())
+                    w = float(x2 - x1)
+                    h = float(y2 - y1)
+                    prop_rect = core.GraphicsRectProperty()
+                    prop_rect.pen_color = [int(c) for c in np.random.choice(range(256), size=3)]
+                    graphics_box = graphics_output.addRectangle(float(x1), float(y1), w, h, prop_rect)
+                    graphics_box.setCategory(self.class_names[cls])
+                    # Label
+                    name = self.class_names[int(cls)]
+                    prop_text = core.GraphicsTextProperty()
+                    prop_text.font_size = 8
+                    prop_text.color = prop_rect.pen_color
+                    graphics_output.addText(name, float(x1), float(y1), prop_text)
+                    # Object results
+                    results = []
+                    confidence_data = dataprocess.CObjectMeasure(
+                        dataprocess.CMeasure(core.MeasureId.CUSTOM, "Confidence"),
+                        float(score),
+                        graphics_box.getId(),
+                        name)
+                    box_data = dataprocess.CObjectMeasure(
+                        dataprocess.CMeasure(core.MeasureId.BBOX),
+                        graphics_box.getBoundingRect(),
+                        graphics_box.getId(),
+                        name)
+                    results.append(confidence_data)
+                    results.append(box_data)
+                    numeric_output.addObjectMeasures(results)
+
+        return instance_seg.cpu().numpy(), panoptic_seg.cpu().numpy()
+
+
+        # Step progress bar:
+        self.emitStepProgress()
+
+        # Call endTaskRun to finalize process
+        self.endTaskRun()
+
+
+# --------------------
+# - Factory class to build process object
+# - Inherits PyDataProcess.CTaskFactory from Ikomia API
+# --------------------
+class InferDetectron2InstanceSegmentationFactory(dataprocess.CTaskFactory):
+
+    def __init__(self):
+        dataprocess.CTaskFactory.__init__(self)
+        # Set process information as string here
+        self.info.name = "infer_detectron2_instance_segmentation"
+        self.info.shortDescription = "your short description"
+        self.info.description = "your description"
+        # relative path -> as displayed in Ikomia application process tree
+        self.info.path = "Plugins/Python"
+        self.info.version = "1.0.0"
+        # self.info.iconPath = "your path to a specific icon"
+        self.info.authors = "algorithm author"
+        self.info.article = "title of associated research article"
+        self.info.journal = "publication journal"
+        self.info.year = 2021
+        self.info.license = "MIT License"
+        # URL of documentation
+        self.info.documentationLink = ""
+        # Code source repository
+        self.info.repository = ""
+        # Keywords used for search
+        self.info.keywords = "your,keywords,here"
+
+    def create(self, param=None):
+        # Create process object
+        return InferDetectron2InstanceSegmentation(self.info.name, param)
